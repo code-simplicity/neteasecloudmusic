65c22aaa070c2e56514973218306181ab88dd958
dpy.cn <468264345@qq.com>
2021-08-25 20:33:35 +0800

    音乐播放器的组件

---
+++ src/components/MianComponent/MusicPlayer/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+      音乐播放器
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MusicPlayer',
+  data () {
+    return {
+
+    }
+  },
+  components: {
+
+  },
+  mounted () {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang='less' scoped>
+
+</style>
