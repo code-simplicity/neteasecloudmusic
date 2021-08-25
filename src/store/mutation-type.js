ab7ef67f7a3b3a9e3f76b3da6b9de1ab3efa7120
dpy.cn <468264345@qq.com>
2021-08-25 20:35:09 +0800

    定义了一些变量

--- src/store/mutation-type.js
+++ src/store/mutation-type.js
@@ -6,7 +6,16 @@ export const LOGIN_STATUS = 'LOGIN_STATUS'
 export const USER_INFO = 'USER_INFO'
 
 // 播放状态
-export const PALYING = 'PALYING'
+export const PLAY_STATUS = 'PLAY_STATUS'
 
 // 播放索引
 export const CURRENT_INDEX = 'CURRENT_INDEX'
+
+// 设置顺序播放列表
+export const SEQUENCE_LIST = 'SEQUENCE_LIST'
+
+// 设置播放列表
+export const PLAY_LIST = 'PLAY_LIST'
+
+// 设置播放模式(循环，单曲，随机)
+export const PLAY_MODE = 'PLAY_MODE'
