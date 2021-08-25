1a3d0563f5ffe30f5939325680b548e1b761bfec
dpy.cn <468264345@qq.com>
2021-08-25 20:36:18 +0800

    定义数据的来源

--- src/store/state.js
+++ src/store/state.js
@@ -1,3 +1,5 @@
+import { playMode } from "../utils/playConfig"
+
 // 数据源
 const state = {
     //登录状态
@@ -7,13 +9,22 @@ const state = {
     userInfo: null,
 
     // 歌手信息
-    singer: {},
+    singerInfo: {},
 
     //播放状态
-    playing: false,
+    playStatus: false,
     
     // 播放列表的索引
     currentIndex: -1,
+
+    // 播放列表
+    playList: [],
+
+    // 顺序播放
+    sequenceList: [],
+
+    // 播放模式
+    mode: playMode.sequence
 }
 
 // 统一暴露
