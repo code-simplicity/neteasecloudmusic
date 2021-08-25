dd32f5070dffacc0c95987247001a3357c5499e0
dpy.cn <468264345@qq.com>
2021-08-25 20:32:29 +0800

    引入播放器的组件

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <main-header></main-header>
     <router-view></router-view>
+    <music-player></music-player>
     <main-footer></main-footer>
   </div>
 </template>
@@ -9,11 +10,13 @@
 <script>
 import MainHeader from './components/MianComponent/MainHeader/index.vue'
 import MainFooter from './components/MianComponent/MainFooter/index.vue'
+import MusicPlayer from './components/MianComponent/MusicPlayer/index.vue'
 export default {
   name: 'App',
   components: {
     MainHeader,
-    MainFooter
+    MainFooter,
+    MusicPlayer
   }
 }
 </script>
