612e5f657f8f633737e7a551a893e3528630dfe7
dpy.cn <468264345@qq.com>
2021-08-30 07:36:13 +0800

    歌词的组件

---
+++ src/components/MianComponent/Lyric/index.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="lyric-box">
+    <Scroll
+      class="lyric"
+      ref="lyricList"
+      :data="currentLyric && currentLyric.lines"
+    >
+      <div class="lyric-wrapper">
+        <div v-if="currentLyric">
+          <p
+            ref="lyricLine"
+            class="lyric-text"
+            :class="currentLyricNum === index ? 'active' : ''"
+            v-for="(item, index) of currentLyric.lines"
+            :key="index"
+          >
+            {{ item.txt }}
+          </p>
+        </div>
+        <div class="no-lyric">暂无歌词,请搜索重试</div>
+      </div>
+    </Scroll>
+  </div>
+</template>
+
+<script>
+import Scroll from '../Scroll/'
+export default {
+    name: 'Lyric',
+    data() {
+        return {
+
+        }
+    },
+    props: {
+        currentLyricNum: {
+            type: Number
+        },
+        currentLyric: {
+            type: Object
+        }
+    },
+    components: {
+        Scroll
+    },
+    mounted() {
+
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style lang="less" scoped>
+.lyric-box {
+  .lyric {
+    display: inline-block;
+    vertical-align: top;
+    width: 100%;
+    height: 430px;
+    overflow: hidden;
+
+    .lyric-wrapper {
+      width: 100%;
+      margin: 0 auto;
+      overflow: hidden;
+      .lyric-text {
+        margin: 5px 0;
+        line-height: 24px;
+        font-size: 14px;
+        font-weight: 300;
+        &:active {
+          color: @color-theme;
+        }
+      }
+    }
+  }
+}
+</style>
