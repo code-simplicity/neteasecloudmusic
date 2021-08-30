8871fd459cb3828a3242f88f56a4dc0b046df819
dpy.cn <468264345@qq.com>
2021-08-30 22:38:51 +0800

    编写歌词接口

--- src/api/service/api.js
+++ src/api/service/api.js
@@ -28,8 +28,15 @@ export const songMusic = () => api.post('/playlist/hot', {})
 export const getSongDetail = (ids,time) => api.post(`/song/detail?ids=${time}`,{ids})
 
 /**
- * 推荐新音乐
+ * 推荐新音乐-最新专辑
  * @param {*} limit 获取推荐新音乐的数量
  * @returns 
  */
 export const getNewSongs = (limit) => api.get(`/personalized/newsong?limit=${limit}`, {})
+
+/**
+ * 获取歌词
+ * @param {*} id 音乐id
+ * @returns 
+ */
+export const getLyric = (id) => api.get(`/lyric?id=${id}`, {})
