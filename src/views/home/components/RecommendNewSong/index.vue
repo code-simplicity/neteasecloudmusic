350c2843a26d9b9be52117bdeb6751c32a8fbe26
dpy.cn <468264345@qq.com>
2021-08-31 22:44:51 +0800

    更改组件显示的title，改为推荐新碟

--- src/views/home/components/RecommendNewSong/index.vue
+++ src/views/home/components/RecommendNewSong/index.vue
@@ -1,20 +1,18 @@
 <template>
-<div class="p-recommend">
+  <div class="p-recommend">
     <div class="p-recommend-header">
-        <span class="p-recommend-text">
-            <i class="iconfont icon-rementuijian-copy"></i>
-            <span>推荐新音乐</span>
-        </span>
-        <span class="p-recommend-more">
-            <span>更多</span>
-            <i class="iconfont icon-gengduo"></i>
-        </span>
+      <span class="p-recommend-text">
+        <i class="iconfont icon-rementuijian-copy"></i>
+        <span>推荐新碟</span>
+      </span>
+      <span class="p-recommend-more">
+        <span>更多</span>
+        <i class="iconfont icon-gengduo"></i>
+      </span>
     </div>
-    <!-- 热门歌单列表,推荐歌单 -->
-    <div>
-        <NewSongList :songMusic="PopularList"></NewSongList>
-    </div>
-</div>
+    <!-- 热门歌单列表,推荐新碟 -->
+    <NewSongList :songMusic="PopularList"></NewSongList>
+  </div>
 </template>
 
 <script>
@@ -90,40 +88,40 @@ export default {
 
 <style lang="less" scoped>
 .p-recommend {
-    margin-top: 20px;
-    padding: 20px;
-    background-color: #fff;
+  margin-top: 20px;
+  padding: 20px;
+  background-color: #fff;
 
-    .p-recommend-header {
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        border-bottom: 1px solid red;
+  .p-recommend-header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    border-bottom: 1px solid red;
 
-        .p-recommend-text {
-            font-size: 1.4rem;
-            float: left;
+    .p-recommend-text {
+      font-size: 1.4rem;
+      float: left;
 
-            .icon-rementuijian-copy {
-                font-size: 1.3rem;
-                color: red;
-                padding-right: 10px;
-            }
-        }
-
-        .p-recommend-more {
-            float: right;
-
-            .icon-gengduo {
-                font-size: 0.8rem;
-                color: red;
-                padding-left: 6px;
-
-                &:hover {
-                    color: #1890ff;
-                }
-            }
-        }
+      .icon-rementuijian-copy {
+        font-size: 1.3rem;
+        color: red;
+        padding-right: 10px;
+      }
     }
+
+    .p-recommend-more {
+      float: right;
+
+      .icon-gengduo {
+        font-size: 0.8rem;
+        color: red;
+        padding-left: 6px;
+
+        &:hover {
+          color: #1890ff;
+        }
+      }
+    }
+  }
 }
 </style>
