0ff0bc8818b68e82ec91248d033bd8c17b60fcad
dpy.cn <468264345@qq.com>
2021-09-01 19:52:28 +0800

    编写歌曲详情的接口

--- src/api/service/api.js
+++ src/api/service/api.js
@@ -20,14 +20,6 @@ export const getPersonalized = (limit) => api.get(`/personalized?limit=${limit}`
 export const songMusic = () => api.post('/playlist/hot', {})
 
 /**
- * 获取歌曲详情
- * @param {*} ids  音乐id,
- * @param {*} time 时间
- * @returns 
- */
-export const getSongDetail = (ids,time) => api.post(`/song/detail?ids=${time}`,{ids})
-
-/**
  * 推荐新音乐-最新专辑
  * @param {*} limit 获取推荐新音乐的数量
  * @returns 
@@ -41,3 +33,19 @@ export const getNewSongs = (limit) => api.get(`/personalized/newsong?limit=${lim
  */
 export const getLyric = id => api.get(`/lyric?id=${id}`, {})
 
+/**
+ * 获取歌单详情
+ * @param {*歌单id} id 
+ * @param {*数量} s 
+ * @param {*当前的时间} time 
+ * @returns 
+ */
+export const getPlayListDetail = (id, s, time) => api.get(`/playlist/detail?id=${id}&s=${s}&timestamp=${time}`, {})
+
+/**
+ * 获取歌曲详情
+ * @param {*} ids 调用此接口 , 传入音乐 id(支持多个 id, 用 , 隔开), 可获得歌曲详情,
+ * @param {*} time 时间
+ * @returns 
+ */
+ export const getSongDetail = (ids, time) => api.post(`/song/detail?timestamp=${time}`, { ids })
