e36ef25d4d5a1376d7a1e5fed2775f337c366131
dpy.cn <468264345@qq.com>
2021-09-02 22:49:28 +0800

    初始化创建评论组件

---
+++ src/components/MianComponent/CommentBox/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>评论组件</div>
+</template>
+
+<script>
+export default {
+  name: 'CommentBox',
+  data() {
+    return {
+
+    }
+  },
+  components: {
+
+  },
+  mounted() {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang='less' scoped>
+</style>
