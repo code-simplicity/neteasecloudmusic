c689273ee4a87a7af0f17377798c32c353ff9871
dpy.cn <468264345@qq.com>
2021-09-06 09:20:21 +0800

    空页面的编写

---
+++ src/components/Empty/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="empty flex-column">
+    <el-image :src="emptyBg" fit="cover"></el-image>
+    <p>{{ emptyText }}</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Empty',
+  data() {
+    return {
+      emptyBg: require('../../assets/images/empty-bg.png')
+    }
+  },
+
+  props: {
+    emptyText: {
+      typr: String,
+      default: '什么都没有，别看了'
+    }
+  },
+  components: {
+
+  },
+  mounted() {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang='less' scoped>
+.empty {
+  width: 100%;
+  .el-image {
+    width: 500px;
+  }
+  p {
+    margin-top: 10px;
+    font-size: 1.3rem;
+    color: @color-blank;
+  }
+}
+</style>
