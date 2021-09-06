17cd61d8331d1d9a7d8261309d495ab64faef761
dpy.cn <468264345@qq.com>
2021-09-06 09:24:30 +0800

    绑定页面的初始化

---
+++ src/views/userupdata/components/BindingSetting/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <!-- 绑定设置 -->
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'BindingSetting',
+  data() {
+    return {
+
+    }
+  },
+  components: {
+
+  },
+  mounted() {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang='less' scoped>
+</style>
