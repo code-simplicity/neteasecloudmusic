303d811419fa6e5c28cbdf4e2731d77e92ca83c8
dpy.cn <468264345@qq.com>
2021-09-06 09:24:55 +0800

    隐私页面的初始化

---
+++ src/views/userupdata/components/PrivacySetting/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <!-- 隐私设置 -->
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'PrivacySetting',
+  data() {
+    return {
+
+    }
+  },
+  components: {
+
+  },
+  mounted() {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang='less' scoped>
+</style>
