7714538d274a4bb7837896ccf5e71aa197bac75b
dpy.cn <468264345@qq.com>
2021-09-08 18:40:24 +0800

    绑定设置的布局编写

--- src/views/userupdata/components/BindingSetting/index.vue
+++ src/views/userupdata/components/BindingSetting/index.vue
@@ -1,6 +1,17 @@
 <template>
   <!-- 绑定设置 -->
-  <div></div>
+  <div class="bind-setting">
+    <div class="bind-top flex-row">
+      <i class="iconfont icon-shouji"></i>
+      <div class="bind-phone">
+        <p>手机</p>
+        <span> 使用手机注册：+86 131****3657修改</span>
+      </div>
+      <div class="bind-btn">
+        <el-button type="primary">修改密码</el-button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -24,4 +35,29 @@ export default {
 </script>
 
 <style lang='less' scoped>
+.bind-setting {
+  display: flex;
+  width: 100%;
+  .bind-top {
+    background: #f5f5f5;
+    align-items: center;
+    height: 120px;
+    padding: 20px 40px;
+    i {
+      font-size: 4rem;
+      color: #e0995e;
+      margin-right: 6px;
+    }
+    .bind-phone {
+      margin-right: 120px;
+      p {
+        font-size: 1.1rem;
+        margin-bottom: 6px;
+      }
+    }
+    .bind-btn {
+      margin-right: 0;
+    }
+  }
+}
 </style>
