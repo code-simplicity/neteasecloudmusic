f239a1a4edc54fa9c94f0b65d3849ce6047f30d6
dpy.cn <468264345@qq.com>
2021-09-08 18:41:13 +0800

    基本设置的编写完成，除了修改头像还存在问题

--- src/views/userupdata/components/UserSetting/index.vue
+++ src/views/userupdata/components/UserSetting/index.vue
@@ -60,14 +60,29 @@
     <div class="user-right">
       <div class="avatar">
         <el-image :src="userProfile.avatarUrl" fit="cover" lazy></el-image>
+        <div class="check-avatar">
+          <span class="check-text" @click="updateAvatarDialog">更换头像</span>
+        </div>
+        <myUpload
+          field="img"
+          @cropSuccess="cropSuccess"
+          @crop-upload-success="cropUploadSuccess"
+          @crop-upload-fail="cropUploadFail"
+          v-model="showImage"
+          :width="300"
+          :height="300"
+          imgFormat="png"
+        ></myUpload>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import myUpload from 'vue-image-crop-upload/upload-2.vue';
 import VDistpicker from 'v-distpicker'
 import { mapGetters } from 'vuex'
+import { avatarUpload } from '../../../../api/service/user'
 export default {
   name: 'UserSetting',
   data() {
@@ -76,7 +91,9 @@ export default {
         nickname: [{
           required: true, message: '请输入活动名称', trigger: 'blur'
         }]
-      }
+      },
+      // 是否展示头衔上传
+      showImage: false,
     }
   },
   props: {
@@ -90,7 +107,8 @@ export default {
   },
 
   components: {
-    VDistpicker
+    VDistpicker,
+    myUpload
   },
 
   computed: {
@@ -102,20 +120,64 @@ export default {
   },
 
   mounted() {
-
   },
   methods: {
+    // 图像上传到服务器的接口
+    cropSuccess(imageDataUrl) {
+      // let imgFile = this.utils.base64ToFile(imgDataUrl, '.png')
+      // console.log('imgFile', imgFile)
+      // eslint-disable-next-line no-debugger
+      debugger
+
+
+      let formData = new FormData();
+      // 使用FormData.append来添加键 / 值对到表单里面;
+      formData.append('imgFile', imageDataUrl);
+      // formData.append('imgSize', 300);
+      // let params = {
+      //   imgFile: imgFile,
+      //   // imgSize: 300
+      // }
+      // let config = {
+      //   headers: { 'Content-Type': 'multipart/form-data' }
+      // }
+      avatarUpload(imageDataUrl).then(res => {
+        if (res.code === this.constants.code_status) {
+          this.userProfile.avatarUrl = res.data.url
+          this.$message.success('头像上传成功')
+          console.log('imageDataUrl', imageDataUrl)
+          console.log('resthis.userProfile.avatarUrlponse', this.userProfile.avatarUrl)
+        }
+      })
+    },
+
+    // 上传成功的回调
+    cropUploadSuccess(response) {
+      if (response.code === this.constants.code_status) {
+        console.log('response', response)
+        this.$message.success('头像上传成功')
+        this.userProfile.avatarUrl = response.data.url
+      }
+    },
+
+    // 上传失败的回调
+    cropUploadFail() {
+      this.$message.error('头像上传失败!')
+    },
+
+    // 修改头像
+    updateAvatarDialog() {
+      // 取反就可以了
+      this.showImage = !this.showImage
+    },
+
     // 选择之后返回值
     getProvince(val) {
-      console.log('val', val)
       this.userProfile.province = val.code
-      console.log(' this.userProfile.city', this.userProfile.city)
     },
 
     getCity(val) {
-      console.log('val1', val)
       this.userProfile.city = val.code
-      console.log('  this.userProfile.city', this.userProfile.city)
     },
 
     saveSubmit(userform) {
@@ -154,8 +216,25 @@ export default {
     display: flex;
     margin-left: 20px;
     .avatar {
-      width: 100px;
-      height: 100px;
+      width: 120px;
+      height: 120px;
+      .check-avatar {
+        right: 0;
+        top: 98px;
+        width: 120px;
+        height: 22px;
+        background: #8e8e8e;
+        position: absolute;
+        text-align: center;
+        opacity: 0.8;
+        span {
+          color: @color-dark;
+          cursor: pointer;
+          &:hover {
+            color: @color-theme;
+          }
+        }
+      }
     }
   }
 }
