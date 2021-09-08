19ddcbad9e15a9b5432af967c46949837700ab68
dpy.cn <468264345@qq.com>
2021-09-08 22:45:27 +0800

    获取视频详情的接口

--- src/api/service/api.js
+++ src/api/service/api.js
@@ -164,3 +164,13 @@ export const getMvAll = params => api.get(`/mv/all`, {
  * @returns 
  */
 export const getMvUrl = id => api.get(`/mv/url?id=${id}`, {})
+
+/**
+ * 获取 mv 数据
+ * 说明 : 调用此接口 , 传入 mvid ( 在搜索音乐的时候传 type=1004 获得 ) , 可获取对应 MV 数据 , 
+ * 数据包含 mv 名字 , 歌手 , 发布时间 , mv 视频地址等数据 , 其中 mv 视频 网易做了防盗链处理 , 
+ * 可能不能直接播放 , 需要播放的话需要调用 ' mv 地址' 接口
+ * @param {*} mvid mvid: mv 的 id
+ * @returns 
+ */
+export const getMvDetail = mvid => api.get(`/mv/detail?mvid=${mvid}`, {})
