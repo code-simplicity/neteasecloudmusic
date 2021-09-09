a51202bd489a2cf55c1ea9cac1230bad6ffb76e1
dpy.cn <468264345@qq.com>
2021-09-09 22:34:07 +0800

    搜索页面的初始化

---
+++ src/views/search/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'Search',
+  data() {
+    return {
+
+    }
+  },
+  components: {
+
+  },
+  mounted() {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang='less' scoped>
+</style>
