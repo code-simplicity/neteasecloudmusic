4fa8ecb74ac2bb3f938f398e7fb90d2bbb1c2657
dpy.cn <468264345@qq.com>
2021-09-10 22:36:25 +0800

    歌手列表头部tab的编写

---
+++ src/views/singer/detail/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'SingerDetail',
+  data() {
+    return {
+
+    }
+  },
+  components: {
+
+  },
+  mounted() {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang='less' scoped>
+</style>
