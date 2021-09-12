2887046484981ea243781e4ca262207760942c80
dpy.cn <468264345@qq.com>
2021-09-12 17:57:20 +0800

    定义歌单接口

--- src/api/service/songer.js
+++ src/api/service/songer.js
@@ -37,4 +37,35 @@ export const getArtisMv = params => api.get(`/artist/mv`, {
  */
 export const getArtistDesc = id => api.get(`/artist/desc?id=${id}`, {})
 
+/**
+ * 获取相似歌手
+ * 说明 : 调用此接口 , 传入歌手 id, 可获得相似歌手
+ * @param {*} id 歌手 id
+ * @returns 
+ */
 export const getSimiArtist = id => api.get(`/simi/artist?id=${id}`, {})
+
+/**
+ * 获取热门歌单分类
+ * @returns 
+ */
+export const playListHot = () => api.get(`/playlist/hot`, {})
+
+/**
+ * 歌单分类
+ * @returns 
+ */
+export const playListCatlist = () => api.get(`/playlist/catlist`, {})
+
+/**
+ * 歌单 ( 网友精选碟 )
+ * 说明 : 调用此接口 , 可获取网友精选碟歌单
+ * @param {*} params order: 可选值为 'new' 和 'hot', 分别对应最新和最热 , 默认为 'hot'
+ * @param {*} params cat: tag, 比如 " 华语 "、" 古风 " 、" 欧美 "、" 流行 ", 默认为 "全部",可从歌单分类接口获取(/playlist/catlist)
+ * @param {*} params limit: 取出歌单数量 , 默认为 50
+ * @param {*} params offset: 偏移数量 , 用于分页 , 如 :( 评论页数 -1)*50, 其中 50 为 limit 的值
+ * @returns 
+ */
+export const getPlayList = params => api.get(`/top/playlist`, {
+    params
+})
