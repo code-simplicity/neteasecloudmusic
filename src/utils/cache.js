1ac95d87750ae641e7788f97d3f86a7cc12fb31f
dpy.cn <468264345@qq.com>
2021-09-12 13:15:47 +0800

    编写历史及的缓存，从storage中获取缓存数据以及删除数据等功能

--- src/utils/cache.js
+++ src/utils/cache.js
@@ -6,6 +6,10 @@ const HISTORY_KEY = '__history__'
 // 历史记录的最大长度
 const HISTORY_MAX_LENGTH = 50
 
+const SEARCH_KEY = '__search__'
+// 搜索保存的最大历史记录
+const SEARCH_MAX = 20
+
 // 插入数组的方法
 function insertArray(array, val, compare, maxLen) {
     // 获取数组下标
@@ -53,5 +57,36 @@ export function deleteHistory(song) {
 // 全部清除最近播放列表
 export function clearHistory() {
     storage.remove(HISTORY_KEY)
+    return []
+}
+
+// 存储历史搜索数据
+export function saveSearch(query) {
+    let searches = storage.get(SEARCH_KEY, [])
+    insertArray(searches, query, item => {
+        return item === query
+    }, SEARCH_MAX)
+    storage.set(SEARCH_KEY, searches)
+    return searches
+}
+
+// 读取搜索历史记录
+export function getSearch() {
+    return storage.get(SEARCH_KEY, [])
+}
+
+// 删除单个历史记录
+export function deleteSearch(query) {
+    let searches = storage.get(SEARCH_KEY, [])
+    deleteFromArray(searches, item => {
+        return item === query
+    })
+    storage.set(SEARCH_KEY, searches)
+    return searches
+}
+
+// 清除所有搜索历史记录
+export function clearSearch() {
+    storage.remove(SEARCH_KEY)
     return []
 }
