eb8eeaa5268f93ae6856cceedba8c0e8cf187724
dpy.cn <468264345@qq.com>
2021-09-15 22:46:13 +0800

    专辑页面的编写

---
+++ src/views/album/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Album',
+  data () {
+    return {
+
+    }
+  },
+  components: {
+
+  },
+  mounted () {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang='less' scoped>
+
+</style>
