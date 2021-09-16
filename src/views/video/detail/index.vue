e67d8ac8679bc3be300f1a98345600e753ef4628
dpy.cn <468264345@qq.com>
2021-09-16 16:49:44 +0800

    视频详情页面的编写

---
+++ src/views/video/detail/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'VideoDetail',
+  data() {
+    return {
+
+    }
+  },
+  components: {
+
+  },
+  mounted() {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang='less' scoped>
+</style>
