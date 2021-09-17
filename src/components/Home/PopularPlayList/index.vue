a2dba668224f47e03f1e33ae21151b29daf56bd9
dpy.cn <468264345@qq.com>
2021-09-17 09:04:05 +0800

    删除一些没必要的注释

--- src/components/Home/PopularPlayList/index.vue
+++ src/components/Home/PopularPlayList/index.vue
@@ -220,8 +220,6 @@ export default {
             top: 0;
             width: 14px;
             height: 24px;
-            // background: url(https://img.alicdn.com/tfs/TB1xEGRub9YBuNjy0FgXXcxcXXa-268-48.png)
-            //   no-repeat 100%;
             background-size: cover;
           }
           .icon-bofangliang {
